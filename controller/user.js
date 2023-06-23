345b8109838a9233a9a719813081f1a56334dc89
Anekant <Anekantjainsagar@gmail.com>
2023-06-23 17:24:05 +0530

    Removed ?

--- controller/user.js
+++ controller/user.js
@@ -16,7 +16,7 @@ exports.userlist = async (req, res) => {
       status: true,
       msg: "success",
       data: findall,
-    })
+    });
   } else {
     res.status(403).json({
       status: false,
@@ -27,52 +27,47 @@ exports.userlist = async (req, res) => {
 };
 
 exports.edituser = async (req, res) => {
-  const{
-    username,
-    email,
-    mobile,
-    image,
-  }=req.body;
+  const { username, email, mobile, image } = req.body;
 
-  data={};
-  if(username){
-    data.username= username;
+  data = {};
+  if (username) {
+    data.username = username;
   }
-  if(email){
+  if (email) {
     data.email = email;
   }
-  if(mobile){
+  if (mobile) {
     data.mobile = mobile;
   }
-  if(image){
+  if (image) {
     data.image = image;
   }
-  if(req.file){
+  if (req.file) {
     const result = await cloudinary.uploader.upload(req.file.path);
-    data.image =  result.secure_url;
+    data.image = result.secure_url;
     fs.unlinkSync(req.file.path);
-}
-  if(data){
-    const updatebrand= await User.findOneAndUpdate(
-        {_id: req.params.id},
-        {$set: data},
-        {new: true}
+  }
+  if (data) {
+    const updatebrand = await User.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: data },
+      { new: true }
     );
-    if(updatebrand){
-        res.status(200).json({
-            status:true,
-            msg: "success",
-            data:updatebrand,
-        })
-    }else{
-        res.status(400).json({
-            status:false,
-            msg:"error",
-            error: "error"
-        });
+    if (updatebrand) {
+      res.status(200).json({
+        status: true,
+        msg: "success",
+        data: updatebrand,
+      });
+    } else {
+      res.status(400).json({
+        status: false,
+        msg: "error",
+        error: "error",
+      });
     }
   }
-}
+};
 
 exports.dlt_user = async (req, res) => {
   try {
@@ -92,7 +87,7 @@ exports.dlt_user = async (req, res) => {
 };
 
 exports.user_true_false = async (req, res) => {
-  const { status } = req.body
+  const { status } = req.body;
   const findupdate = await User.findOneAndUpdate(
     { _id: req.params.id },
     { $set: { status: status } },
@@ -103,16 +98,15 @@ exports.user_true_false = async (req, res) => {
       status: true,
       msg: "success",
       status: findupdate.status,
-    })
+    });
   } else {
     res.status(403).json({
       status: false,
       msg: "error",
       error: error,
-    })
+    });
   }
-
-}
+};
 
 exports.getviewone = async (req, res) => {
   try {
@@ -122,19 +116,18 @@ exports.getviewone = async (req, res) => {
         status: true,
         msg: "success",
         data: findone,
-      })
-    }
-    else {
+      });
+    } else {
       res.status(403).json({
         status: false,
         msg: "error",
         error: "error",
-      })
+      });
     }
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 exports.sendotp = async (req, res) => {
   let length = 6;
@@ -143,7 +136,7 @@ exports.sendotp = async (req, res) => {
   const { mobile } = req.body;
   const newUser = new User({
     mobile: mobile,
-    otp: defaultotp
+    otp: defaultotp,
   });
 
   const findexist = await User.findOne({ mobile: mobile });
@@ -152,26 +145,27 @@ exports.sendotp = async (req, res) => {
       status: false,
       msg: "welcome back",
       data: findexist,
-    })
+    });
   }
-  newUser.save()
+  newUser
+    .save()
     .then((data) => {
       res.status(200).json({
         status: true,
         msg: "otp send successfully",
         data: data.mobile,
         otp: data.otp,
-        _id: data?._id,
-      })
+        _id: data._id,
+      });
     })
     .catch((error) => {
       res.status(400).json({
         status: false,
         msg: "unsend otp",
         error: error,
-      })
-    })
-}
+      });
+    });
+};
 
 exports.verifyotps = async (req, res) => {
   let length = 6;
@@ -185,7 +179,7 @@ exports.verifyotps = async (req, res) => {
         msg: "otp verified please register",
         mobile: mobile,
         otp: defaultotp,
-        _id: findone._id
+        _id: findone._id,
       });
     }
   } else {
@@ -194,67 +188,70 @@ exports.verifyotps = async (req, res) => {
       msg: "Incorrect Otp",
     });
   }
-}
+};
 
 exports.userRegister = async (req, res) => {
-  const { status, password } = req.body
+  const { status, password } = req.body;
   const salt = bcrypt.genSaltSync(saltRounds);
   const hashpassword = bcrypt.hashSync(password, salt);
 
-
   const userdetail = await User.findOneAndUpdate(
     {
-      _id: req.params.id
+      _id: req.params.id,
     },
-    { $set: req.body, status: "true", password: hashpassword, cnfrmPassword: hashpassword },
+    {
+      $set: req.body,
+      status: "true",
+      password: hashpassword,
+      cnfrmPassword: hashpassword,
+    },
     { new: true }
-  )
+  );
   if (userdetail) {
     res.status(200).json({
       status: true,
       msg: "success",
       data: userdetail,
-    })
+    });
   } else {
     res.status(400).json({
       status: false,
       msg: "error",
       error: "error",
-    })
+    });
   }
-}
+};
 
-
-exports.totaluser = async(req, res)=>{
+exports.totaluser = async (req, res) => {
   await User.countDocuments()
-  .then((data)=>{
-    res.status(200).json({
-      status: true,
-      data: data
+    .then((data) => {
+      res.status(200).json({
+        status: true,
+        data: data,
+      });
     })
-  })
-  .catch((error)=>{
-    res.status(400).json({
-      status: false,
-      msg: "error",
-      error: "error",
-    })
-  })
-}
+    .catch((error) => {
+      res.status(400).json({
+        status: false,
+        msg: "error",
+        error: "error",
+      });
+    });
+};
 
 exports.resetPassword = async (req, res) => {
-  const { oldpassword ,password, cnfrmPassword } = req.body
-  const userData = await User.findOne({_id : req.params.id})
-  if(userData){
-  const passwordMatch = await bcrypt.compare(oldpassword,userData.password)
-  if(passwordMatch){
-    // console.log("matched")
-    if(password === cnfrmPassword){
-      const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(password, salt);
-    const findandUpdateEntry = await User.findOneAndUpdate(
+  const { oldpassword, password, cnfrmPassword } = req.body;
+  const userData = await User.findOne({ _id: req.params.id });
+  if (userData) {
+    const passwordMatch = await bcrypt.compare(oldpassword, userData.password);
+    if (passwordMatch) {
+      // console.log("matched")
+      if (password === cnfrmPassword) {
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
+        const findandUpdateEntry = await User.findOneAndUpdate(
           {
-            _id: req.params.id
+            _id: req.params.id,
           },
           { $set: { password: hashPassword, cnfrmPassword: hashPassword } },
           { new: true }
@@ -264,40 +261,28 @@ exports.resetPassword = async (req, res) => {
             status: true,
             msg: "success",
             data: findandUpdateEntry,
-            
           });
-        } 
-    }else {
-      res.status(401).json({
+        }
+      } else {
+        res.status(401).json({
+          status: false,
+          msg: "Password confirm password not matched",
+        });
+      }
+    } else {
+      res.status(400).json({
         status: false,
-        msg: "Password confirm password not matched"
-        
+        msg: "Old Password not matched",
       });
     }
-  }else{
-    res.status(400).json({
-          status: false,
-          msg: "Old Password not matched",
-         
-        })
-      }
-  
   }
 };
 
 exports.adduser = async (req, res) => {
-  const {
-    username,
-    email,
-    mobile,
-    password,
-    cnfrmPassword,
-    status,
-  } = req.body;
+  const { username, email, mobile, password, cnfrmPassword, status } = req.body;
 
   const salt = bcrypt.genSaltSync(saltRounds);
   const hashpassword = bcrypt.hashSync(password, salt);
-
 
   if (password == cnfrmPassword) {
     const newadduser = new user({
@@ -307,50 +292,50 @@ exports.adduser = async (req, res) => {
       password: hashpassword,
       cnfrmPassword: hashpassword,
       status: "true",
-    })
+    });
 
-    const findexist = await user.findOne({ email: email})
+    const findexist = await user.findOne({ email: email });
     if (findexist) {
       res.status(403).json({
         status: false,
         msg: "Allready exist",
         data: {},
-      })
+      });
     }
-    newadduser.save()
+    newadduser
+      .save()
       .then((newadduser) => {
         res.status(200).json({
           status: true,
           msg: "success",
           data: newadduser,
-        })
+        });
       })
       .catch((error) => {
         res.status(400).json({
           status: false,
           msg: "error",
           error: error,
-        })
-      })
-  }else{
-      res.status(403).json({
-        status: false,
-        msg: "password and cnfrmpassword is not match",
-        error: "error",
-      })
+        });
+      });
+  } else {
+    res.status(403).json({
+      status: false,
+      msg: "password and cnfrmpassword is not match",
+      error: "error",
+    });
   }
-}
-
+};
 
 exports.login = async (req, res) => {
-  const {  mobile, password,email } = req.body;
-  const user = await User.findOne( {   
+  const { mobile, password, email } = req.body;
+  const user = await User.findOne({
     $and: [
-      { $or: [{ mobile: mobile }, { email: email }] }, { $or: [{status:"true" }] }
-    ]
+      { $or: [{ mobile: mobile }, { email: email }] },
+      { $or: [{ status: "true" }] },
+    ],
   });
   if (user) {
-      
     const validPass = await bcrypt.compare(password, user.password);
     if (validPass) {
       res.status(200).send({
